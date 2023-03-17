74ce1db1e0f023e039197260462547c56edf8176
harry-sketch <harshbisht90@gmail.com>
2023-03-17 23:57:06 +0530

    add: cards

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,7 @@
-import { Navbar } from 'nextra-theme-docs/.';
-import '../styles/global.css';
+import "../styles/global.css";
 
 export default function Nextra({ Component, pageProps }) {
-  return (
-    <>
-      <Component {...pageProps} />
-    </>
-  );
+  const getLayout =
+    (Component as any).getLayout || ((page: React.ReactElement) => page);
+  return <>{getLayout(<Component {...pageProps} />)}</>;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./app/**/*.{js,ts,jsx,tsx,md,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,md,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,md,mdx}",
   ],
   theme: {
     extend: {},
