db9cd498462ecac8309348679f46eb9ae18636ef
harry-sketch <harshbisht90@gmail.com>
2023-03-18 15:39:16 +0530

    add button styling

--- components/common/Button/Button.tsx
+++ components/common/Button/Button.tsx
@@ -20,9 +20,12 @@ export const Button: React.FC<Props> = ({
 
   return (
     <button
-      type="button"
       onClick={() => (isRouting ? push(url) : window.open(url))}
-      className={cn(variant === "primary" ? "" : "", className, "")}
+      className={cn(
+        variant === "primary" ? "customPrimaryButton" : "customSecondaryButton",
+        className,
+        "transition-all duration-300 ease-in-out h-14 w-auto border rounded-lg font-bold text-xl"
+      )}
     >
       {text}
     </button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,15 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,md,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundSize: {
+        "size-200": "200% 200%",
+      },
+      backgroundPosition: {
+        "pos-0": "0% 0%",
+        "pos-100": "100% 100%",
+      },
+    },
   },
   plugins: [],
 };
